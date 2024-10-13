2fe7bf06a0d98bbf7dd63572d026371d868ae3da
xiangbiao2009 <xiangbiao2009@163.com>
2024-10-13 08:56:14 +0800

    class load update

--- com/jack/jvm/MyClassLoaderTest.java
+++ com/jack/jvm/MyClassLoaderTest.java
@@ -8,11 +8,13 @@ import java.lang.reflect.Method;
 public class MyClassLoaderTest{
 
     public static void main(String[] args) throws Exception {
+        // 默認加載 D:/test/com/jack/jvm/User1
         MyClassLoader classLoader = new MyClassLoader("D:/test");
         Class<?> aClass = classLoader.loadClass("com.jack.jvm.User1");
         Object obj = aClass.newInstance();
         Method sout = aClass.getDeclaredMethod("sout", null);
         Object invoke = sout.invoke(obj, null);
         System.out.println(aClass.getClassLoader().getClass().getName());
+        System.out.println(aClass.getClassLoader().getParent().getClass().getName());
     }
 }
